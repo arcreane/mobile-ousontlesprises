c3e21a7a22bbf84fd6c3218a14ddbf63dbd6146d
wassimtns14 <75323257+wassimtns14@users.noreply.github.com>
2021-11-27 18:40:41 +0100

    Create AActivity.java

---
+++ app/src/main/java/com/example/wassim/projet_jeux/AActivity.java
@@ -0,0 +1,21 @@
+package com.example.wassim.projet_jeux;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Wassim on 14/11/2021.
+ */
+
+public class AActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((R.layout.jeu));
+
+    }
+}
